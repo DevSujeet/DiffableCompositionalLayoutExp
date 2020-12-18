2b90d56dd49878cd4cc8406173b1a0ed3a5569ef
Sujeet Kumar <sujeetk@fractalanalytics.com>
2020-12-18 10:56:12 +0530

    Initial Commit

---
+++ DiffableComposotionalLayoutTests/DiffableComposotionalLayoutTests.swift
@@ -0,0 +1,33 @@
+//
+//  DiffableComposotionalLayoutTests.swift
+//  DiffableComposotionalLayoutTests
+//
+//  Created by Sujeet.Kumar on 18/12/20.
+//
+
+import XCTest
+@testable import DiffableComposotionalLayout
+
+class DiffableComposotionalLayoutTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
